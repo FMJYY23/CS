c66eaef5f000faddc1d9da4d5475827b0cb9e52c
FMJYY23 <62998134+FMJYY23@users.noreply.github.com>
2022-10-18 17:01:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i "s/OpenWrt /OPWRT/g" package/lean/default-settings/files/zzz-default-sett
 
 sed -i 's/R22.8.2/Pro 22.10.1/g' package/lean/default-settings/files/zzz-default-settings
 
-sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透设置"/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
+#sed -i 's/"Frp 内网穿透"/"超级服务器外网穿透设置"/g' feeds/luci/applications/luci-app-frpc/po/zh-cn/frp.po
 
 sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
 
